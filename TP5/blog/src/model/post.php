b319069ae970cc0e5dae4c29d96001d2f30a62de
Nicolas1s <115209939+Nicolas1s@users.noreply.github.com>
2023-11-08 10:52:19 +0100

    Tirez parti de la composition

--- TP5/blog/src/model/post.php
+++ TP5/blog/src/model/post.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('src/lib/database.php');
+
 class Post
 {
     public $title;
@@ -7,34 +9,34 @@ class Post
     public $content;
     public $identifier;
 }
-
 class PostRepository
 {
-    public $database = null;
-
+    public $connection;
     public function getPost(string $identifier): Post
     {
-        $this->dbConnect();
-        $statement = $this->database->prepare(
-            "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts WHERE id = ?"
+        
+        $statement = $this->connection->getConnection()->prepare(
+            "SELECT id, title, content, DATE_FORMAT(creation_date,
+            '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts
+            WHERE id = ?"
         );
-        $statement->execute([$identifier]);
 
+        $statement->execute([$identifier]);
         $row = $statement->fetch();
         $post = new Post();
         $post->title = $row['title'];
         $post->frenchCreationDate = $row['french_creation_date'];
         $post->content = $row['content'];
         $post->identifier = $row['id'];
-
         return $post;
     }
-
     public function getPosts(): array
     {
-        $this->dbConnect();
-        $statement = $this->database->query(
-            "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
+        
+        $statement = $this->connection->getConnection()->query(
+            "SELECT id, title, content, DATE_FORMAT(creation_date,
+            '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts
+            ORDER BY creation_date DESC LIMIT 0, 5"
         );
         $posts = [];
         while (($row = $statement->fetch())) {
@@ -43,17 +45,9 @@ class PostRepository
             $post->frenchCreationDate = $row['french_creation_date'];
             $post->content = $row['content'];
             $post->identifier = $row['id'];
-
             $posts[] = $post;
         }
-
         return $posts;
     }
-
-    public function dbConnect()
-    {
-        if ($this->database === null) {
-            $this->database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-        }
-    }
+    
 }
